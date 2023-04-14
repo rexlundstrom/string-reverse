c53ffeeede8be42ca8e340928bd5a7ded72da9fa
Rex Lundstrom <rexlundstrom@gmail.com>
2023-04-14 16:14:17 -0400

    full functionality and consistency across pages

--- js/site.js
+++ js/site.js
@@ -1,10 +1,38 @@
-function displayMessage() {
-  let msg = document.getElementById("message").value;
-  // alert(msg);
+// controller function
+const getValues = () => {
+  // get the user's input
+  let userInput = document.getElementById("message").value;
+  // send it through the reversed string function
+  let reversedInput = reverseString(userInput);
 
-  Swal.fire({
-    backdrop: false,
-    title: "APP NAME",
-    text: msg,
-  });
+  displayString(reversedInput);
 }
+
+// business logic
+const reverseString = (message) => {
+  // declare empty string
+  let output = '';
+
+  for (let i = message.length - 1; i >= 0; i--) {
+    output += message[i];
+  }
+  // iterate over current string
+  return output;
+}
+
+// view function
+const displayString = (reversedMessage) => {
+  // display string on the page 
+  document.getElementById("msg").textContent = reversedMessage;
+  document.getElementById("alert").classList.remove('d-none');
+}
+
+
+// let msg = document.getElementById("message").value;
+  // // alert(msg);
+
+  // Swal.fire({
+  //   backdrop: false,
+  //   title: "String Reverse",
+  //   text: msg,
+  // });
